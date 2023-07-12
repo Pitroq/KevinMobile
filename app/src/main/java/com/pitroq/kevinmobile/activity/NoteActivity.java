28c19c6ca9dc0ca91e36f5c86f502915092cd1c0
Pitroq <piotr3k420@gmail.com>
2023-07-12 16:13:48 +0200

    add reading notes

--- app/src/main/java/com/pitroq/kevinmobile/activity/NoteActivity.java
+++ app/src/main/java/com/pitroq/kevinmobile/activity/NoteActivity.java
@@ -1,13 +1,16 @@
 package com.pitroq.kevinmobile.activity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.pitroq.kevinmobile.Note;
 import com.pitroq.kevinmobile.Notepad;
 import com.pitroq.kevinmobile.R;
 
 public class NoteActivity extends AppCompatActivity {
     private Notepad notepad;
+    private int noteId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +19,19 @@ public class NoteActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.note_title);
 
         notepad = new Notepad(this);
+        notepad.loadNotesFromFile();
 
-        String noteId = getIntent().getStringExtra("noteId");
-        System.out.println("NOTE: " + noteId);
+        noteId = Integer.parseInt(getIntent().getStringExtra("noteId"));
+        loadNote();
+    }
+
+    private void loadNote() {
+        Note note = notepad.getNote(noteId);
+        ((EditText) findViewById(R.id.noteTitleEditText)).setText(note.getTitle());
+
+        String noteContent = note.getNoteContent();
+        noteContent = noteContent.replace("{enter}", "\n");
+        ((EditText) findViewById(R.id.noteContentEditText)).setText(noteContent);
     }
 
 }
